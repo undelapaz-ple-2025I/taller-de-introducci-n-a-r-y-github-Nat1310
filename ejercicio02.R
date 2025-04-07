e22063e637f20cbda2b3922c55791c9959f11726
Natalia Martinez Caceres <natmartinezca@unal.edu.co>
2025-04-07 09:03:47 -0500

    Update ejercicio02.R

--- ejercicio02.R
+++ ejercicio02.R
@@ -2,8 +2,8 @@
 
 solucion <- function() {
   # Asigna valores a 'a' y 'b' de manera que el valor asignado a 's' sea 100
-  a <- NULL # asigna valor aquí
-  b <- NULL # asigna valor aquí
+  a <- 230 # asigna valor aquí
+  b <- 130 # asigna valor aquí
   s <- a-b
   return(s)
 }
